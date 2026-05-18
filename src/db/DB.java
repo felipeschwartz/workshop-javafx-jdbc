3cb3815bb0fbd1837437d86719a819d48e6771e0
Felipe Schwartz <felipeschwartz@gmail.com>
2026-05-18 18:42:27 -0300

    Building JAR file

--- src/db/DB.java
+++ src/db/DB.java
@@ -2,6 +2,7 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -36,14 +37,17 @@ public class DB {
 			}
 		}
 	}
-	
+
 	private static Properties loadProperties() {
-		try (FileInputStream fs = new FileInputStream("db.properties")) {
+		try {
+			InputStream is = DB.class.getClassLoader().getResourceAsStream("db.properties");
+			if (is == null) {
+				throw new DbException("Arquivo db.properties não encontrado!");
+			}
 			Properties props = new Properties();
-			props.load(fs);
+			props.load(is);
 			return props;
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			throw new DbException(e.getMessage());
 		}
 	}
